32a50c38cc785cf36cb149a4a58a073a65f664d4
Jeff Ripke <jripke74@gmail.com>
2017-12-12 16:23:03 -0800

    added tableview methods

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -10,5 +10,13 @@ import UIKit
 
 class ChecklistViewController: UITableViewController {
     
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
+        return cell
+    }
 }
 
