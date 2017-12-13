ff94cdb832e90d039722d98bfee9e79cac86654a
Jeff Ripke <jripke74@gmail.com>
2017-12-12 16:12:42 -0800

    Initail commit

---
+++ Checklists/ChecklistViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Checklists
+//
+//  Created by Jeff Ripke on 12/12/17.
+//  Copyright © 2017 Jeff Ripke. All rights reserved.
+//
+
+import UIKit
+
+class ChecklistViewController: UITableViewController {
+    
+}
+
